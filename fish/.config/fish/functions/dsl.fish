4c4f67112dca2f4ef84a9c4d9a9dcd2c5007112e
catmousedog <lauwersarno@hotmail.com>
2026-08-26 10:41:02 +0200

    vscode settings

--- fish/.config/fish/functions/dsl.fish
+++ fish/.config/fish/functions/dsl.fish
@@ -1,3 +1,3 @@
 function dsl
-	ds9 $argv -lock frame wcs -lock scale yes -lock colorbar yes -log -cmap Heat -match frame wcs
+	ds9 $argv -lock frame wcs -lock scale yes -lock colorbar yes -lock slice image -log -cmap Heat -match frame wcs
 end
